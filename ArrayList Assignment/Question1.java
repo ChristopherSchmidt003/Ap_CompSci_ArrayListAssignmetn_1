b4e57616aaeb43e06a46be0eca0ce7d01303e929
Christopher Schmidt <56738166+ChristopherSchmidt003@users.noreply.github.com>
2021-01-11 09:51:29 -0800

    Add files via upload

---
+++ ArrayList Assignment/Question1.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+class Question1 {
+    
+    public static ArrayList<String> reverse( ArrayList<String> words ) {
+        //This method takes an ArrayList<String> as a parameter and returns
+        //a new ArrayList<String> in which the elements are stored in reverse
+        //order.  The original list should remain unchanged.
+        ArrayList<String> reverse = new ArrayList<String>();
+        for (int i = words.size()-1; i >= 0; i--) {
+            reverse.add(words.get(i));
+        }
+        return reverse;
+    }
+    
+    public static void main (String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Why");
+        words.add("do");
+        words.add("we");
+        words.add("fall");
+        words.add("Bruce?");
+        words.add("So");
+        words.add("we");
+        words.add("can");
+        words.add("learn");
+        words.add("to");
+        words.add("pick");
+        words.add("ourselves");
+        words.add("up");
+        System.out.println("List forwards reads: " + words);
+        System.out.println("List backward reads: " + reverse(words));
+    }
+    
+    
+}
